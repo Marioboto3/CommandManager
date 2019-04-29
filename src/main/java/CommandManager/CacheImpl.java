5787640885223ac158754fd9313576a921835e3f
Marioboto3 <marioboto98@gmail.com>
2019-04-29 12:46:23 +0200

    Acabado

--- src/main/java/CommandManager/CacheImpl.java
+++ src/main/java/CommandManager/CacheImpl.java
@@ -2,14 +2,26 @@ package CommandManager;
 
 import java.util.HashMap;
 
+import java.util.HashMap;
+
 public class CacheImpl implements Cache {
 
+    private HashMap<String,Command> commands;
 
+    private static CacheImpl ourInstance = new CacheImpl();
+
+    public static CacheImpl getInstance() {
+        return ourInstance;
+    }
+
+    private CacheImpl() {
+        commands = new HashMap<String, Command>();
+    }
     public Command get(String key) {
-        return null;
+        return this.commands.get(key);
     }
 
     public void put(String key, Command c) {
-
+        this.commands.put(key,c);
     }
 }
--- src/main/java/CommandManager/Factory.java
+++ src/main/java/CommandManager/Factory.java
@@ -3,4 +3,13 @@ package CommandManager;
 import java.util.HashMap;
 
 public class Factory {
+    public static Command newInstance(String commandType) throws ClassNotFoundException, IllegalAccessException, InstantiationException{
+        Cache cache = CacheImpl.getInstance();
+        Command c = cache.get(commandType);
+        if(c==null){
+            c = (Command) Class.forName("upc.edu.dsa."+commandType).newInstance();
+            cache.put(commandType,c);
+        }
+        return c;
+    }
 }
